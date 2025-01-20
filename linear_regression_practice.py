2d49eefa944d0b6bc48555204e05852e174675f1
Will Gasser <willbg04@gmail.com>
2025-01-20 16:27:22 -0600

    base h0

--- linear_regression_practice.py
+++ linear_regression_practice.py
@@ -5,24 +5,33 @@ import csv
 boston_house_data_csv = "Boston.csv"
 
 # Glossary:
-# crime_rate: Crime rate by town
+# crime_rate: Per capita crime rate by town
 # residential_zone: Proportion of residential land zoned for lots over 25,000 sq. ft.
 # non_retail_business_acre: Proportion of non-retail business acres per town
-# charles_river: Dummy variable indicating if the house is on the Charles River (aesthetic value)
-# nitric_oxide_concentration: Concentration of nitric oxides (pollution indicator)
+# charles_river: Dummy variable indicating if the tract bounds the Charles River (1 if yes, 0 otherwise)
+# nitric_oxide_concentration: Concentration of nitric oxides (parts per 10 million)
 # average_rooms: Average number of rooms per dwelling
 # age: Proportion of owner-occupied units built prior to 1940
 # distance_to_employment_centers: Weighted distances to five Boston employment centers
 # radial_highways_access: Index of accessibility to radial highways
 # tax_rate: Full-value property-tax rate per $10,000
 # pupil_teacher_ratio: Pupil-teacher ratio by town
+# proportion_black: Proportion of the population that is African American
+# lower_status_population: Percentage of the population with lower socioeconomic status
+# median_value: Median value of owner-occupied homes in $1000s (target variable)
 
-houses = []
 
-with open(boston_house_data_csv, 'r'):
-    reader = csv.reader(boston_house_data_csv, delimiter=',')
-    for row in reader:
-        houses.append(row)
+
+def build_data(fileName: str) -> list[list[float]]:
+    with open(fileName, 'r'):
+        houses = []
+        reader = csv.reader(boston_house_data_csv, delimiter=',')
+        for row in reader:
+            houses.append(row)
+        return houses
+        
+        
+h0 =  
     
 
 
